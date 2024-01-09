c637e901ce3730136cee746d7dc81120225a6357
SARFRAZ  BAGWAN <137871744+ImBsarfraz@users.noreply.github.com>
2024-01-09 16:45:26 +0530

    my portfolio

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu_icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onClick = () => {
+    menuIcon.classList.toggle('fa-x');
+    navbar.classList.toggle('active');
+};
+
+// scroll sections active link
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                 links.classList.remove('active');
+                 document.querySelector('header nav a[href*='+ id + ']').classList.add('active');
+            });
+        };
+    });
+    // sticky navbar
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+};
